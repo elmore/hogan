f7daa500749f9c4754768ec87ee83d83e10c81ee
tom elmore <elmore_tom@hotmail.com>
2013-09-20 16:59:15 +0100

    initial commit of hogan

---
+++ hogan.js
@@ -0,0 +1,35 @@
+;(function ( $, window, document, undefined ) {
+    
+    var pluginName = 'defaultPluginName',
+        defaults = {
+            propertyName: "value"
+        };
+		
+    // The actual plugin constructor
+    function Plugin( element, options ) {
+        this.element = element;
+
+        this.options = $.extend( {}, defaults, options) ;
+        
+        this._defaults = defaults;
+        this._name = pluginName;
+        
+        this.init();
+    }
+
+    Plugin.prototype.init = function () {
+		
+    };
+
+    // A really lightweight plugin wrapper around the constructor, 
+    // preventing against multiple instantiations
+    $.fn[pluginName] = function ( options ) {
+        return this.each(function () {
+            if (!$.data(this, 'plugin_' + pluginName)) {
+                $.data(this, 'plugin_' + pluginName, 
+                new Plugin( this, options ));
+            }
+        });
+    }
+
+})( jQuery, window, document );
